fb02894ab8fc80fc6fed20edef9490183e573e40
Oshadhi-Anjana <oshadhianjana@gmail.com>
2026-02-20 12:11:02 +0530

    feat: implement PHPUnit tests for UserModel and update bootstrap configuration

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,10 +1,25 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class UserModelTest extends TestCase
 {
-    public function testTrue()
+    public function testBootstrapLoaded()
     {
-        $this->assertTrue(true);
+        // Test that the bootstrap was loaded successfully
+        $this->assertTrue(defined('BASEPATH'));
+        $this->assertTrue(defined('APPPATH'));
+    }
+
+    public function testGetInstanceExists()
+    {
+        // Test that get_instance function is available
+        $this->assertTrue(function_exists('get_instance'));
+    }
+
+    public function testCanGetCIInstance()
+    {
+        // Test that we can get a CI instance
+        $CI = get_instance();
+        $this->assertIsObject($CI);
     }
 }
