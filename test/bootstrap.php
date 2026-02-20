8971d4c65695ffcbef2fad4e4e7185dfb3c22385
Oshadhi-Anjana <oshadhianjana@gmail.com>
2026-02-20 12:00:11 +0530

    feat: add PHPUnit configuration and example test files

---
+++ test/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+// Load CI3 framework for tests
+define('BASEPATH', __DIR__ . '/../system/');
+define('APPPATH', __DIR__ . '/../application/');
+
+require_once __DIR__ . '/../system/core/CodeIgniter.php';
